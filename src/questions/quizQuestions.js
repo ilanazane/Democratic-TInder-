9d9105c092d75a0a06b38b12acde7f5e30e72aeb
Ilana Zane <46580323+ilanazane@users.noreply.github.com>
2020-02-02 11:43:45 -0500

    Create quizQuestions.js

---
+++ src/questions/quizQuestions.js
@@ -0,0 +1,139 @@
+var quizQuestions=[
+  {
+    question: "Which form of healthcare do you support? ",
+    answers:[
+      {
+        type:" Bernie Sanders",
+        content:"Tax the middle class for healthcare "
+      },
+      {
+        type: "Elizabeth Warren",
+        content:"Tax the wealthy for healthcare"
+      },
+      {
+        type:"Pete Buttigieg",
+        content: "Something between private and public insurance"
+      },
+      {
+        type:"Joe Biden",
+        content:"Obamacare"
+      },
+      {
+        type: "Michael Bloomberg",
+        content: "Expand Obamacare and Medicare, not Medicare for All"
+      }
+    ]
+  },
+  {
+    question:"Is is important to close the wealth gap?",
+    answers: [
+      {
+        type: "Elizabeth Warren",
+        content: "Yes, but don't tax millionairs too much...only billionaires"
+      },
+      {
+        type: "Bernie Sanders",
+        content:"Yes, tax millionairs/billionairs steeply"
+      },
+      {
+        type: "Joe Biden",
+        content: "I'm not interested in seeing billionars heavily taxed"
+      },
+      {
+        type: "Pete Buttigieg",
+        content: "We can close the wealth gap by raising minimum wage to $15/hour"
+      },
+      {
+        type: "Michael Bloomberg",
+        content:"We need to boost black business, black home ownership and invest in poor neighborhoods"
+      }
+  ]
+},
+{
+  question: "Do you think everyone should go to college for free?",
+  answers:[
+    {
+      type:"Elizabeth Warren",
+      content: "Yes and we can get the money by taxing the wealthy"
+    },
+    {
+      type:"Bernie Sanders",
+      content: "Yes and we can get the money by taxing Wall Street"
+    },
+    {
+      type: "Joe Biden",
+      content: "Only community college should be free"
+    },
+    {
+      type:"Pete Buttigieg",
+      content:"No, only families earning less than $100,000 should recieve free tuition"
+    },
+    {
+      type:"Michael Bloomberg",
+      content: "No, but we should have lower interst rates on student loans"
+    },
+  ]
+},
+{
+  question: "What are your thoughts on legalizing marijuana?",
+  answers:[
+    {
+      type: "Elizabeth Warren",
+      content: "There is great racial disparity in marijuana related arrests so it should be legalized"
+    },
+    {
+      type: "Joe Biden",
+      content: "Every state should decide on their own"
+    },
+    {
+      type: "Bernie Sanders",
+      content: "Legalize it and $500 million should be invested into communites most affected by the Drug War"
+    },
+    {
+      type: "Michael Bloomberg",
+      content:"Marijuana should not be legalized"
+    },
+    {
+      type: "Pete Buttigieg",
+      content: "Just decriminalize all drug posession"
+    }
+  ]
+},
+{
+  question: "Do you want to see a president that will make history based on their race, gender, sexual orientation or religion?",
+  answers:[
+    {
+      type: " Elizabeth Warren",
+      content: "I want to see a woman as president"
+    },
+    {
+      type: "Pete Buttigieg",
+      content:"I want to vote for someone of different sexual orientation"
+    },
+    {
+      type: " Bernie Sanders",
+      content: "I would vote for a president of different religion"
+    },
+    {
+      type: "Joe Biden",
+      content:" I don't have a preference"
+    },
+  ]
+},
+{
+  question: "Do you mind if the next president has a billion dollar net worth?",
+  answers:[
+    {
+      type: "Pete Buttigieg",
+      content:"Yes"
+    },
+    {
+      type:"Michael Bloomberg",
+      content:"No"
+    }
+  ]
+}
+
+];
+
+export default quizQuestions;
